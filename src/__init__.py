42668214bfa8946aae63c0b989c1a481cfecfd2b
Bowen Zeng <b27zeng@uwaterloo.ca>
2026-01-03 13:03:44 -0800

    Update package description in __init__.py
    Expanded the package description to include cleaning processes.

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 """
 Co-op Salary Analysis - Source Code Package
 
-This package contains the core data processing and analysis modules.
+This package contains the core data processing and analysis modules which I use to do the cleaning
 """
 
 __version__ = "0.1.0"
